9fb4a1c39b021aa6b220eb945c08a974a9208421
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-04-09 18:46:50 +0530

    Add files via upload

---
+++ Array-countgivenPairSum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int a[], int n, int k)
+{
+    map<int, int> mp;
+    int ct = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int rem = k - a[i];
+        if (mp.find(rem) != mp.end())
+        {
+            ct = ct + mp[rem];
+        }
+        mp[a[i]]++;
+    }
+
+
+    for(auto it:mp){
+        cout<<it.first<<" "<<it.second<<"\n";
+    }
+    return ct;
+}
+int main()
+{
+
+    int n = 4, k = 2;
+    int arr[] = {1, 1, 1, 1};
+
+    cout << solve(arr, n, k);
+    return 0;
+}
