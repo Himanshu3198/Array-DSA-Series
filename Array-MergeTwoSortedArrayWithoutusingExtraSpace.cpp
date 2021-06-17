e4cc2e82fc794833e62755e0a8bcafc38621f710
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-06-17 23:07:16 +0530

    Update Array-MergeTwoSortedArrayWithoutusingExtraSpace.cpp

--- Array-MergeTwoSortedArrayWithoutusingExtraSpace.cpp
+++ Array-MergeTwoSortedArrayWithoutusingExtraSpace.cpp
@@ -53,3 +53,44 @@ int main(){
     merge(arr1,arr2,n,m);
     return 0;
 }
+
+
+
+//  method 2  gapping method  time complexity  O(nlogn)
+class Solution{
+    public:
+        //Function to merge the arrays.
+
+        
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            // code here 
+                    int i, j, gap = (n + m)/2+(n+m)%2;
+    while(gap>0) 
+    {
+        // comparing elements in the first array.
+        for (i = 0; i + gap < n; i++)
+            if (arr1[i] > arr1[i + gap])
+                swap(arr1[i], arr1[i + gap]);
+ 
+        // comparing elements in both arrays.
+        for (j = gap > n ? gap - n : 0; 
+             i < n && j < m;
+             i++, j++)
+            if (arr1[i] > arr2[j])
+                swap(arr1[i], arr2[j]);
+ 
+        if (j < m) {
+            // comparing elements in the second array.
+            for (j = 0; j + gap < m; j++)
+                if (arr2[j] > arr2[j + gap])
+                    swap(arr2[j], arr2[j + gap]);
+        }
+        
+        if(gap<=1){
+            gap=0;
+        }
+        gap=(gap)/2+(gap)%2;
+    }
+        }
+};
