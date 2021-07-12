55abfbf6150e76019e7301678ffe11bb3932b71f
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-07-12 20:39:33 +0530

    Update Array-Sliding-window-maxium-average-subarray.cpp

--- Array-Sliding-window-maxium-average-subarray.cpp
+++ Array-Sliding-window-maxium-average-subarray.cpp
@@ -1,12 +1,24 @@
 // 643. Maximum Average Subarray I
 
-double sum=0, res=INT_MIN;
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum=0, res=INT_MIN;
+        
         for(int i=0;i<nums.size();i++) {
             if(i<k) sum+=nums[i];
             else {
+            
                 res=max(sum, res);
-                sum+=nums[i]-nums[i-k];
+                
+                sum+=nums[i];
+                sum-=nums[i-k];
             }
+         
         }
         res=max(sum, res);
         return res/k;
+        
+     
+    }
+};
