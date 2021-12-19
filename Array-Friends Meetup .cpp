d07f11562493fb71e8c2e090ea1b5dc3f049abd3
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-12-19 23:07:59 +0530

    Create Array-Friends Meetup .cpp

---
+++ Array-Friends Meetup .cpp
@@ -0,0 +1,66 @@
+// https://www.codechef.com/COOK136C/problems/FRIMEET/
+#include <bits/stdc++.h>
+#define ll long long int
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, n) for (int i = (n); i >= 0; i--)
+#define fst first
+#define snd second
+#define E "\n"
+#define pb push_backCF
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007LL
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define setbits(x) __builtin_popcount(x)
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+
+void solve()
+{
+
+
+     ll x1,x2;
+     cin>>x1>>x2;
+
+    
+     if(x1==x2){
+         cout<<"YES\n";
+         return;
+     }
+     if(x2>x1){
+         cout<<"NO\n";
+         return;
+     }
+
+        if(x1>x2){
+              cout<<"YES\n";
+               return;
+        }
+        
+
+         cout<<"NO\n";
+         return;  
+
+  
+}
+
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+
+        solve();
+    }
+
+    return 0;
+}
