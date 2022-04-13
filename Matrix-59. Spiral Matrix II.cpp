1d928ad1059c8f9b44d209f541ec5b1fedcd54fb
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-04-14 00:12:07 +0530

    Update Matrix-59. Spiral Matrix II.cpp

--- Matrix-59. Spiral Matrix II.cpp
+++ Matrix-59. Spiral Matrix II.cpp
@@ -2,37 +2,27 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         
-         vector<vector<int>>result;
-         
-        
-          int number=1;
-         
+           vector<vector<int>>result(n,vector<int>(n));
+           int number=1;
            int top=0,left=0;
            int bottom=n-1,right=n-1;
-        
-          while(number*number){
+           while(number<=n*n){
+                    for(int i=left;i<=right;i++)
+                       result[top][i]=number++;
               
+                   top++;
+                   for(int i=top;i<=bottom;i++)
+                      result[i][right]=number++;
               
-              for(int i=left;i<=right;i++){
-                  
-                  result[top][i]=number++;
-              }
-              top++;
+                    right--;
+                    for(int i=right;i>=left;i--)
+                      result[bottom][i]=number++;
               
-              for(int i=top;i<=bottom;i++){
-                  result[i][right]=number++;
-              }
-              right--;
-              
-              for(int i=right;i>=left;i--){
-                  result[bottom][i]=number++;
-              }
-              bottom++;
-              for(int i=bottom;i>=top;i--){
-                  result[i][left]=number++;
-                  left++;
-              }
-                 
+                    bottom--;
+                    for(int i=bottom;i>=top;i--)
+                     result[i][left]=number++;
+               
+                    left++;
           }
          return result;
     }
