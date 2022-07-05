387d8a2d5c2bd394a4d9f3426086d5b0ab416814
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-07-05 21:29:29 +0530

    Update Array-LongestConsecutiveSequence.cpp

--- Array-LongestConsecutiveSequence.cpp
+++ Array-LongestConsecutiveSequence.cpp
@@ -1,50 +1,24 @@
-
-// longest consecutive sequence in array in c++;
-
-#include<bits/stdc++.h>
-using namespace std;
-
-
-void LCS(vector<int>&nums){
-    sort(nums.begin(),nums.end());
-    int count=0;
-    int max_length=INT_MIN;
-
-
-            int p1,p2=0;
-    for(int p1=1;p1<nums.size();p1++){
-
-        if(nums[p1]!=nums[p2]){
-            p2++;
-            nums[p2]=nums[p1];
-        }
-    } 
-
-
-    nums.resize(p2+1);
-
-    for(int i=0;i<nums.size();i++){
-
-
-        if( i>0 && nums[i]==nums[i-1]+1){
-            count++;
-        }
-        else{
-        
-            count=1;
-        }
-
-        max_length=max(max_length,count);
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        if(n<2) return n;
+        int diff=nums[1]-nums[0],res=1,len=1;
+        // for(auto it:nums) cout<<it<<" ";
+        // cout<<"\n";
+        for(int i=1;i<n;i++){
+         if(nums[i]==nums[i-1]){
+             continue;
+         }
+         int curr=nums[i-1]+1;
+            if(curr==nums[i]){
+                len++;
+            }else{
+                len=1;
+            }
+            res=max(res,len); 
+      }
+        return res;
     }
-
-    cout<<max_length<<"\n";
-}
-
-
-int main(){
-
-    vector<int> v={100,4,200,1,3,2};
-
-    LCS(v);
-    return 0;
-}
+};
