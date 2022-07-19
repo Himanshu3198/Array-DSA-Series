1508060d04ec7bf90b03a48cf8e22cd9f07e2445
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-07-19 21:51:30 +0530

    Update Array-Pascal-triangle.cpp

--- Array-Pascal-triangle.cpp
+++ Array-Pascal-triangle.cpp
@@ -1,24 +1,23 @@
-// 118. Pascal's Triangle
-
-
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>res(numRows);
         
         
-        for(int i=0;i<numRows;i++){
+        vector<vector<int>>res;
+        if(numRows>=1) res.push_back({1});
+        if(numRows>=2) res.push_back({1,1});
+        vector<int>last={1,1};
+        if(numRows<=2) return res;
+        for(int n=3;n<=numRows;n++){
+            vector<int>v(n,1);
             
-            res[i].resize(i+1);
-            res[i][0]=res[i][i]=1;
-            
-            for(int j=1;j<i;j++){
-                res[i][j]=res[i-1][j-1]+res[i-1][j];
+            for(int i=0;i<v.size();i++){
+                  if(i==0 || i==(v.size()-1)) continue;
+                 v[i]=last[i]+last[i-1];
             }
+            last=v;
+            res.push_back(v);
         }
-        
         return res;
-        
-        
     }
 };
