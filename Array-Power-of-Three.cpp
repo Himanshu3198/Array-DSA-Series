71454f562f2cc37c76480fa8ef258e65f798ae9d
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-08-24 21:44:51 +0530

    Update Array-Power-of-Three.cpp

--- Array-Power-of-Three.cpp
+++ Array-Power-of-Three.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
      
-        
-        for(long long int i=1;i<=n;i=i*3){
-            
-            if(i==n){
-                return true;
-            }
-        }
-        
+        for(long long i=1;i<=n;i=i*3) if(i==n)return true;
         return false;
     }
 };
