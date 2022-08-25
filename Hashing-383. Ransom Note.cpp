c5ecbdf917784343799685acc3482278965ddd79
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-08-25 20:07:01 +0530

    Update Hashing-383. Ransom Note.cpp

--- Hashing-383. Ransom Note.cpp
+++ Hashing-383. Ransom Note.cpp
@@ -3,25 +3,12 @@ public:
     bool canConstruct(string nums, string magazine) {
         
         
-         map<char,int>mp;
-        
-          for(char c:magazine){
-              
-              
-              mp[c]++;
+          map<char,int>mp;
+          for(char c:magazine) mp[c]++;
+          for(int  i=0;i<nums.size();i++){
+            if(mp[nums[i]])  mp[nums[i]]--;
+            else return false;
           }
-        
-           int n=nums.size();
-        for(int  i=0;i<n;i++){
-            
-            if(mp[nums[i]]){
-                mp[nums[i]]--;
-            }
-            else{
-                return false;
-            }
-        }
-        
-        return true;
+          return true;
     }
 };
